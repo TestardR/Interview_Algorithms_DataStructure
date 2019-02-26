9e57e38939a326ad3f33b56b17c2d6cb12393ff8
unknown <rmtestard@gmail.com>
2019-02-26 13:55:57 +0100

    % modulus

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,24 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    // Is the number a multiple of 3 and 5 ?
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+      // Is the number a multiple of 3 ?
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+      // Is the number a multiple of 5 ?
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+      // Otherwise print values
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz(100);
 
 module.exports = fizzBuzz;
