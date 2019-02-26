ad7752dfa6266f1dde31315fc036d5e18b472bbf
unknown <rmtestard@gmail.com>
2019-02-26 14:28:26 +0100

    slice !

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// Solution 2
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
+
+// Solution 1
+// function chunk(array, size) {
+//   // create an empty array to hold chunks
+//   const chunked = [];
+//   // for each element in array ('unchunked')
+//   for (let element of array) {
+//     // retrieve the last array in 'chunked'
+//     const last = chunked[chunked.length - 1];
+//     // if there is not a last array or if the last array has the size asked, lets push the element in another array
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//   return chunked;
+// }
+
+console.log(chunk([1, 2, 3, 4], 3));
 
 module.exports = chunk;
