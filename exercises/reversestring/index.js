ad999fc2aa70eeafd85946ca4e658d3a79aa671a
unknown <rmtestard@gmail.com>
2019-02-26 09:35:38 +0100

    reverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reversedString = str
+    .split('')
+    .reverse()
+    .join('')
+    .replace(',', '');
+  //   console.log(reversedString);
+  return reversedString;
+}
+
+// reverse('hello');
 
 module.exports = reverse;
