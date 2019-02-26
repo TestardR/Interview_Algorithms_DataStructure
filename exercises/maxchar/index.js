e5d2124c97b357806a046eaf313f0314aae0ce4c
unknown <rmtestard@gmail.com>
2019-02-26 13:43:39 +0100

    iteration through object vs. array

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,45 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+/* Similar questions : 
+    What is the most common character in the string ?
+    Does string A have the same characters as string B (is it an anagram) ?
+    Does the given string have any repeated characters in it ?
+*/
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+  // for of is used to iterate through an array or a string
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  // for in is used to iterate through an object
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+// function maxChar(str) {
+//     const chars = {};
+
+//     for (let char of str) {
+//       if (!chars[char]) {
+//         chars[char] = 1;
+//       } else {
+//         chars[char]++;
+//       }
+//     }
+//     return chars;
+//   }
+
+console.log(maxChar('Hel22lo111'));
 
 module.exports = maxChar;
