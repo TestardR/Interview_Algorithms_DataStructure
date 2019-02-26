9245490f2bcbdb67d800edb134ae870f903d7649
unknown <rmtestard@gmail.com>
2019-02-26 13:17:52 +0100

    reverse number

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = String(n)
+    .split('')
+    .reverse()
+    .join('');
+  return parseInt(reversed) * Math.sign(n);
+}
+console.log(reverseInt(-15));
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//     const reversed = n
+//       .toString()
+//       .split('')
+//       .reverse()
+//       .join('');
+
+//     return parseInt(reversed) * Math.sign(n);
+//   }
+//   console.log(reverseInt(500))
