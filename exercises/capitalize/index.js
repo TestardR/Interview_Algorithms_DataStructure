8a4dda7d99ec8755af3a54ec1295564995c4d14a
unknown <rmtestard@gmail.com>
2019-02-27 09:47:07 +0100

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,13 +8,28 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 // Solution 1
-function capitalize(str) {
-  const words = [];
-  for (let word of str.split(' ')) {
-    words.push(word[0].toUpperCase() + word.slice(1));
-  }
+// function capitalize(str) {
+//   const words = [];
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
 
-  return words.join(' ');
+//   return words.join(' ');
+// }
+
+// Solution 2
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
 }
 
+console.log(capitalize('a short sentence'));
 module.exports = capitalize;
