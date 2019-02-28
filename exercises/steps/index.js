b632d480aa3f6c77283de05789ecc32476976a98
unknown <rmtestard@gmail.com>
2019-02-28 08:39:22 +0100

    recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,25 +17,61 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  // From 0 to n (iterate through rows)
-  for (let row = 0; row < n; row++) {
-    // Create an empty string, 'stair'
-    let stair = '';
-    // From 0 to n (iterate through columns)
-    for (let column = 0; column < n; column++) {
-      // If the current column is equal to or less than the current row
-      if (column <= row) {
-        // Add a '#' to 'stair'
-        stair += '#';
-      } else {
-        // Add a space to 'stair'
-        stair += ' ';
-      }
-    }
-    console.log(stair);
+function steps(n, row = 0, stair = '') {
+  // if (n === row), we have reached the end of our problem
+  if (n === row) {
+    return;
   }
+  // if 'stair' string has a length === n, then we are at the end of a row
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+  // if the length of the stair string is less than or equal to the row number, we add a '#', otherwise add a space
+  // if (stair.length <= row) {
+  //   stair += '#';
+  // } else {
+  //   stair += ' ';
+  // }
+  const add = stair.length <= row ? '#' : ' ';
+
+  // call the process again
+  steps(n, row, stair + add);
+  // steps(n, row, stair);
 }
+
+// Exemple recursion
+/* function printNumber(n, dec = 1) {
+  if (n === 0) {
+    return;
+  }
+  console.log(n);
+  printNumber(n - dec);
+}
+
+printNumber(10); */
+
+// Solution 1
+// function steps(n) {
+//   // From 0 to n (iterate through rows)
+//   for (let row = 0; row < n; row++) {
+//     // Create an empty string, 'stair'
+//     let stair = '';
+//     // From 0 to n (iterate through columns)
+//     for (let column = 0; column < n; column++) {
+//       // If the current column is equal to or less than the current row
+//       if (column <= row) {
+//         // Add a '#' to 'stair'
+//         stair += '#';
+//       } else {
+//         // Add a space to 'stair'
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
 
 console.log(steps(8));
 
