bc5b0ec2a1703cecfc98871800ca4b87dba75d3f
unknown <rmtestard@gmail.com>
2019-03-01 13:54:13 +0100

    memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -27,6 +27,27 @@ function fib(n) {
   return fib(n - 1) + fib(n - 2);
 }
 
-console.log(fib(6));
+// Solution 2 # Memoization
+function memoize(fn) {
+  // store calls to fast versions and respective results
+  const cache = {};
+  // assign the arguments given to slowFib and assign them to an array
+  return function(...args) {
+    // if a result has been registered, return it
+    if (cache[args]) {
+      return cache[args];
+    }
+    // if passed the if, it means that it is the first time that this result is called
+    // call slow fib, whenever we call a function with an array of arguments with have to use the apply helper
+    const result = fn.apply(this, args); // here = fn(args)
+    // at the key args, store the result we just created
+    cache[args] = result;
+    return result;
+  };
+}
+
+fib = memoize(fib);
+
+// console.log(fib(6));
 
 module.exports = fib;
