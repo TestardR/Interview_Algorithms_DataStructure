8d2e13646c66034a932d974e148d6c2839de3d26
unknown <rmtestard@gmail.com>
2019-03-06 12:42:21 +0100

    mergeSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -36,7 +36,18 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  const center = Math.floor(arr.length / 2);
+  // everything from to not included center
+  const left = arr.slice(0, center);
+  // everything from center
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
 
 function merge(left, right) {
   const results = [];
